fcbcd81ab6ee3eee34822699a3697d6b423e59c6
RuGrof <RuGrof@gmail.com>
2015-12-23 16:25:19 +0500

    added webpack and modify template declaration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,16 @@
+import '../styles/main.css';
+
 (function () {
+   var templates={};
+  templates['main'] = require('../templates/main.mustache');
+  templates['project-list'] = require('../templates/project-list.mustache');
+  console.log("2");
   'use strict';
   var TemplateView = Backbone.View.extend({
     render: function () {
       var template = templates[this.template];
-      this.$el.html(template.render(this.getContext()));
+      //this.$el.html(template.render(this.getContext()));
+      this.$el.html(template(this.getContext()));
     },
     getContext: function () {
       return this.model ? this.model.toJSON() : {};
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports={
+    context: __dirname + "/app",
+    entry: {
+        main : "./scripts/main",
+          
+    },
+    output: {
+         path: __dirname + "/public",
+         publicPath: "/",
+         filename: "[name].js"
+    },
+    watch: NODE_ENV == 'development',
+    
+    devtool: NODE_ENV == 'development' ? "source-map" : null,
+    plugins:[
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify( NODE_ENV )
+        }),
+        /*new webpack.optimize.CommonsChunkPlugin({
+            
+        }),*/
+        new webpack.ProvidePlugin({
+            
+        }),
+        new ExtractTextPlugin('styles.css', {allChunks: true})
+    ],
+    module:{
+        
+        loaders: [{
+            test:   /\.js$/,
+            exclude: /\/node_modules\//,
+            loader: 'babel?presets[]=es2015'
+        },{
+            test: /\.mustache$/,
+            exclude: /\/node_modules\//,
+            loader: 'mustache'
+        },{
+            test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+            loader: 'file?name=[path][name].[ext]'
+        },{
+            test:   /\.css$/,
+            loader: ExtractTextPlugin.extract('css!')
+        }]
+    },
+    devServer: {
+
+    host: 'localhost', // default
+    port: 8081,
+    contentBase: __dirname+ '/devServer'
+
+  }
+};
+
+if(NODE_ENV === 'production'){
+    module.exports.plugins.push(
+        new webpack.optimize.UnglifyJsPlugin({
+            compress:{
+                warnings:false,
+                drop_console: true
+            }
+        })
+    );
+}
